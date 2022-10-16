9967d945ebb435db12676195fd1db02cd0368137
oswaldohernandez <oswaldohernandez@protonmail.com>
2022-10-16 15:26:09 -0500

    feat convert pulses to degrees

--- angular_position_control/src/main.cpp
+++ angular_position_control/src/main.cpp
@@ -6,18 +6,29 @@ struct Encoder
 {
   uint8_t channel_A;
   uint8_t channel_B;
-  uint32_t counter;
+  int32_t counter;
   uint32_t last_state_A;
   uint32_t current_state_A;
   uint32_t current_state_B;
+
 } Encoder_1;
 
+struct Motor
+{
+  uint32_t current_position;
+  uint32_t target_position;
+  uint8_t pwm_pin;
+} Motor_1;
+
 /** TODO
- * Read encoder check
- * Convert pulses to degrees
- * Implement P controller on degrees
- * Send u to PWM
+ * [x] Read encoder
+ * [] Convert pulses to degrees
+ * [] Implement P controller on degrees
+ * [] Send u to PWM
  */
+void init_encoder(Encoder &encoder);
+void update_encoder_state(Encoder &encoder);
+uint32_t convert_pulses_to_degrees(int32_t pulse_counter);
 
 void setup()
 {
@@ -34,6 +45,7 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   update_encoder_state(Encoder_1);
+  Motor_1.current_position = convert_pulses_to_degrees(Encoder_1.counter);
 }
 
 void init_encoder(Encoder &encoder)
@@ -63,4 +75,17 @@ void update_encoder_state(Encoder &encoder)
     Serial.println(encoder.counter);
   }
   encoder.last_state_A = encoder.current_state_A;
+}
+
+uint32_t convert_pulses_to_degrees(int32_t pulse_counter)
+{
+  uint32_t pulses_per_revolution = 630; // Change according to motor
+  uint32_t total_degrees = 360;
+  if (pulse_counter < 0)
+  {
+    pulse_counter = pulses_per_revolution + pulse_counter;
+  }
+
+  uint32_t angle = total_degrees * pulse_counter / pulses_per_revolution;
+  return angle;
 }
