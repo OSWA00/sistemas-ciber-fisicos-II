2b58d8bda2d765f8217a0dd3c32627a2f1cd5bff
oswaldohernandez <oswaldohernandez@protonmail.com>
2022-10-16 17:42:59 -0500

    feat send power

--- angular_position_control/src/main.cpp
+++ angular_position_control/src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-#include "control_parameters.h"
-#include "pins.h"
 
 struct Encoder
 {
@@ -15,20 +13,30 @@ struct Encoder
 
 struct Motor
 {
-  uint32_t current_position;
-  uint32_t target_position;
-  uint8_t pwm_pin;
+  float_t current_position;
+  float_t target_position;
+  float_t proportional_gain;
+  bool foward;
+  uint8_t foward_pin;
+  uint8_t reverse_pin;
+  uint8_t enable_pin;
 } Motor_1;
 
 /** TODO
  * [x] Read encoder
- * [] Convert pulses to degrees
- * [] Implement P controller on degrees
+ * [x] Convert pulses to degrees
+ * [x] Implement P controller on radians
  * [] Send u to PWM
+ *
+ * After controller synthonization
+ * [] Receive reference from external source
  */
 void init_encoder(Encoder &encoder);
+void init_motor(Motor &motor);
 void update_encoder_state(Encoder &encoder);
-uint32_t convert_pulses_to_degrees(int32_t pulse_counter);
+float_t convert_pulses_to_radians(int32_t pulse_counter);
+float_t calculate_u(Motor &motor);
+void send_power(Motor &motor, float_t speed);
 
 void setup()
 {
@@ -38,14 +46,20 @@ void setup()
   Encoder_1.channel_A = 0x1;
   Encoder_1.channel_B = 0X2;
 
+  Motor_1.foward_pin = 0x1B;
+  Motor_1.reverse_pin = 0x1A;
+  Motor_1.enable_pin = 0xE;
+
   init_encoder(Encoder_1);
+  init_motor(Motor_1);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   update_encoder_state(Encoder_1);
-  Motor_1.current_position = convert_pulses_to_degrees(Encoder_1.counter);
+  Motor_1.current_position = convert_pulses_to_radians(Encoder_1.counter);
+  send_power(Motor_1, calculate_u(Motor_1));
 }
 
 void init_encoder(Encoder &encoder)
@@ -77,15 +91,67 @@ void update_encoder_state(Encoder &encoder)
   encoder.last_state_A = encoder.current_state_A;
 }
 
-uint32_t convert_pulses_to_degrees(int32_t pulse_counter)
+float_t convert_pulses_to_radians(int32_t pulse_counter)
 {
   uint32_t pulses_per_revolution = 630; // Change according to motor
-  uint32_t total_degrees = 360;
+  float_t total_radians = 2 * M_PI;
   if (pulse_counter < 0)
   {
     pulse_counter = pulses_per_revolution + pulse_counter;
   }
 
-  uint32_t angle = total_degrees * pulse_counter / pulses_per_revolution;
+  uint32_t angle = total_radians * pulse_counter / pulses_per_revolution;
   return angle;
+}
+
+float_t calculate_u(Motor &motor)
+{
+  float_t angle_error = motor.target_position - motor.current_position;
+  motor.foward = angle_error < M_PI; // Check if angle_error > 180
+
+  if (!motor.foward)
+  {
+    angle_error = angle_error - M_PI;
+  }
+
+  float_t u = -motor.proportional_gain * angle_error;
+  return u;
+}
+
+void init_motor(Motor &motor)
+{
+
+  pinMode(motor.foward_pin, OUTPUT);
+  pinMode(motor.reverse_pin, OUTPUT);
+  pinMode(motor.enable_pin, OUTPUT);
+
+  uint32_t freq = 0x3E8;
+  uint8_t channel = 0x0;
+  uint8_t resolution_bits = 0x10;
+
+  ledcSetup(channel, freq, resolution_bits);
+  ledcAttachPin(motor.enable_pin, channel);
+
+  digitalWrite(motor.foward_pin, LOW);
+  digitalWrite(motor.reverse_pin, LOW);
+}
+
+void send_power(Motor &motor, float_t u)
+{
+  uint8_t pwm_channel = 0x0;
+
+  if (motor.foward)
+  {
+    digitalWrite(motor.foward_pin, HIGH);
+    digitalWrite(motor.reverse_pin, LOW);
+  }
+  else
+  {
+    digitalWrite(motor.foward_pin, LOW);
+    digitalWrite(motor.reverse_pin, HIGH);
+  }
+
+  uint32_t duty_cycle = map(u, 0x0, 0x1, 0x0, 0xFFFF);
+
+  ledcWrite(pwm_channel, duty_cycle);
 }
